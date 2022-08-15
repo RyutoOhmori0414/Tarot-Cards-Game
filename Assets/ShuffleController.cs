62ac40bf3fb1b82f1c8bd19afdfe6ed84ed04dac
RyutoOhmori <ryutoomori0414@gmail.com>
2022-08-15 14:14:20 +0900

    システムの完成、後組み上げるだけ

--- Assets/ShuffleController.cs
+++ Assets/ShuffleController.cs
@@ -2,27 +2,41 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShuffleController : MonoBehaviour
 {
     /// <summary>何秒で特定の位置まで動くのか</summary>
-    [Header("最初の移動の秒数")]
+    [Header("移動の秒数")]
     [SerializeField] float _moveTime = default;
     /// <summary>二次移動の時に使うanchor</summary>
     [Header("次の移動のアンカー")]
-    [SerializeField] Transform _anchor = default;
+    [SerializeField] RectTransform _anchor = default;
+    /// <summary>x座標のランダムの絶対値</summary>
+    [Header("x座標のランダムの絶対値")]
+    [SerializeField] float _xRandom = default;
+    /// <summary>y座標のランダムの絶対値</summary>
+    [Header("y座標のランダムの絶対値")]
+    [SerializeField] float _yRanom = default;
+    /// <summary>角度ランダムの最高値</summary>
+    [Header("角度ランダムの絶対値")]
+    [SerializeField] float _rotationRanom = default;
     /// <summary>tureの間、移動が行われる</summary>
     bool _isShuffled1 = false;
     /// <summary>二次移動が行われる</summary>
     bool _isShuffled2 = false;
+    /// <summary>三次移動が行われる</summary>
+    bool _isShuffled3 = false;
     /// <summary>最終的な位置</summary>
     Vector3 _targetP = Vector3.zero;
     /// <summary>最終的な角度</summary>
     Vector3 _targetR = Vector3.zero;
-    /// <summary>二次移動の際のベクトル</summary>
-    Vector3 _secondMove = Vector3.zero;
-    //タイマー
+    /// <summary>タイマー</summary>
     float _timer = default;
+    /// <summary>このオブジェクトのRectTracsform</summary>
+    RectTransform _rectTransform;
+    /// <summary>移動前の位置</summary>
+    Vector3 _before;
     private void OnEnable()
     {
         GameManager.Shuffle += MoveCard1;
@@ -30,9 +44,11 @@ public class ShuffleController : MonoBehaviour
     }
     void Start()
     {
+        _rectTransform = GetComponent<RectTransform>();
+        _before = _rectTransform.position;
         this.transform.localScale = Vector3.one;
-        _targetP = new Vector3(UnityEngine.Random.Range(-6.0f, 6.0f), UnityEngine.Random.Range(-2.5f, 2.5f), 0);
-        _targetR = new Vector3(0, 0, UnityEngine.Random.Range(0.0f, 90.0f));
+        _targetP = new Vector3(UnityEngine.Random.Range(-_xRandom * 1.0f, _xRandom * 1.0f), UnityEngine.Random.Range(-_yRanom * 1.0f, _yRanom * 1.0f), 0f);
+        _targetR = new Vector3(0, 0, UnityEngine.Random.Range(0.0f, UnityEngine.Random.Range(0.0f, _rotationRanom) * 1.0f));
     }
     // Update is called once per frame
     void Update()
@@ -41,24 +57,53 @@ public class ShuffleController : MonoBehaviour
         if (_isShuffled1)
         {
             _timer += Time.deltaTime;
-            this.transform.Translate(_targetP.x / (60 * _moveTime), _targetP.y / (60 * _moveTime), 0, Space.World);
-            this.transform.Rotate(0, 0, _targetR.z / (60 * _moveTime));
-
-            if (_timer > _moveTime)
+            
+            if (_timer <= _moveTime)
             {
+                float rate = Mathf.Clamp01(_timer / _moveTime);
+                _rectTransform.position = Vector3.Lerp(_before, _targetP, rate);
+            }
+            else
+            {
+                _rectTransform.position = _targetP;
+                _before = _rectTransform.position;
                 _isShuffled1 = false;
                 _timer = 0;
-                _secondMove = _anchor.position - this.transform.position;
+                _isShuffled2 = true;
             }
         }
-        if (_isShuffled2)
+        else if (_isShuffled2)
         {
             _timer += Time.deltaTime;
-            this.transform.Translate(_secondMove.x / (60 * _moveTime), _secondMove.y / (60 * _moveTime), 0, Space.World);
 
-            if (_timer > _moveTime)
+            if (_timer <= _moveTime)
             {
+                float rate = Mathf.Clamp01(_timer / _moveTime);
+                _rectTransform.position = Vector3.Lerp(_before, _anchor.position, rate);
+            }
+            else
+            {
+                _rectTransform.position = _anchor.position;
+                _before = _rectTransform.position;
                 _isShuffled2 = false;
+                _timer = 0;
+                _isShuffled3 = true;
+            }
+        }
+        else if (_isShuffled3)
+        {
+            _timer += Time.deltaTime;
+
+            if (_timer <= _moveTime)
+            {
+                float rate = Mathf.Clamp01(_timer / _moveTime);
+                _rectTransform.position = Vector3.Lerp(_before, Vector3.zero, rate);
+            }
+            else
+            {
+                _rectTransform.position = Vector3.zero;
+                _before = _rectTransform.position;
+                _isShuffled3 = false;
                 _timer = 0;
             }
         }
